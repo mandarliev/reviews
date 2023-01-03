c8cac2f226bf370ae7ea6a3271032df1c1c3f098
Dimitar <dimitar.mandarliev@gmail.com>
2023-01-03 11:29:02 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,49 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Nikola Mechkata',
+    job: 'rich unemployed',
+    image:
+      'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/304935220_817670669669735_2077062277140689719_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_ohc=IWaChGxgwAgAX9fRqFf&_nc_ht=scontent.fsof8-1.fna&oh=00_AfC9Z0ic1G6dZRhneWT2PDipUT28hO7xNnaQXQpFs0SZsA&oe=63B8CC78',
+    text:
+      "I was simping before it was cool",
+  },
+  {
+    id: 2,
+    name: 'Mitacheto',
+    job: 'web developer',
+    image:
+      'https://scontent.fsof11-1.fna.fbcdn.net/v/t1.6435-1/137627333_394748528419832_6075247196256208930_n.jpg?stp=c0.28.160.160a_dst-jpg_p160x160&_nc_cat=110&ccb=1-7&_nc_sid=7206a8&_nc_ohc=QrjqQVf2I2EAX-shXUL&_nc_ht=scontent.fsof11-1.fna&oh=00_AfCM3uw_JZ9GJo2WU05DSXWpRErb4OjATuHExzbAeFSiSw&oe=63DB550B',
+    text:
+      'УМРИ УШЕВ :)',
+  },
+  {
+    id: 3,
+    name: 'Gavril',
+    job: 'TPM Bratleee',
+    image:
+      'https://scontent.fsof11-1.fna.fbcdn.net/v/t1.6435-9/87394946_2974971922566010_6985624681852174336_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=KdTic8ZqIL4AX_TWEKq&_nc_ht=scontent.fsof11-1.fna&oh=00_AfAiPooqW84os94ZNJBRoWslJ3r7kX7b_ZW_ahBNeQNhLw&oe=63DB7E50',
+    text:
+      'Sieg Heil!',
+  },
+  {
+    id: 4,
+    name: 'Stanyaka',
+    job: 'the boss',
+    image:
+      'https://scontent.fsof11-1.fna.fbcdn.net/v/t1.6435-9/34258933_1796283130432870_125332909543391232_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=xIKT5vujI94AX-XwvDg&_nc_ht=scontent.fsof11-1.fna&oh=00_AfDZTNybHnjdCCWeu6Camwwbc1oltq3fXo6kwGGLIwI3CA&oe=63DB4B84',
+    text:
+      'Действаме си!',
+  },
+  {
+    id: 5,
+    name: 'Moikata',
+    job: 'the хипар',
+    image:
+      'https://scontent.fsof11-1.fna.fbcdn.net/v/t1.6435-9/89846905_2913388685350440_2480394581816901632_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=akyM2qKMhfgAX-nvNsY&_nc_ht=scontent.fsof11-1.fna&oh=00_AfBB68TxYYsDXnDZseLveMUHQyMze_vW2VHx1FlfTLVRtA&oe=63DB6F8F',
+    text:
+      'Ъммм, да',
+  }
+];
+
+export default reviews;
